aaa6f9bd1540d13b9f3bb79b710f14993e38265e
DiogoJacomini1 <dijacomini@gmail.com>
2023-11-13 08:22:30 -0300

    DepartmentDaoJDBC findById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -3,6 +3,7 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 
@@ -10,10 +11,17 @@ public class Program2 {
 		// TODO Auto-generated method stub
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("\n==== TEST 1: Department insert====");
+		System.out.println("==== TEST 1: Department findById====");
+		Department department = departmentDao.findById(4);
+		System.out.println(department);
+		
+		System.out.println("\n==== TEST 2: Department insert====");
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Insert! New Id:" + newDepartment.getId());
+		
+	
+
 	}
 
 }
